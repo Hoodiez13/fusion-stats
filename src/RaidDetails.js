450a8c46f6e9b664498f1a183c8c7442cb930f34
samtrextom <46818719+samtrextom@users.noreply.github.com>
2021-09-27 21:26:05 -0500

    Update RaidDetails.js

--- src/RaidDetails.js
+++ src/RaidDetails.js
@@ -10,6 +10,8 @@ const RaidDetails = () => {
     const {id} = useParams()
 
     const [players, setPlayers] = useState([])
+    //const [startTime, setStartTime] = useState(0)
+    //const [endTime, setEndTime] = useState(0)
 
     const getRaidInfo = () =>{
         axios.request({
@@ -23,20 +25,55 @@ const RaidDetails = () => {
             query:`{
               reportData {
                 report(code:"${id}"){
-                    fights{
-                      name
-                    }
+                  startTime
+                  endTime
                     rankedCharacters{
                       name
                     }
+                  masterData{
+                    abilities{
+                      gameID
+                      name
+                      type
+                    }
+                  }
                 }
               }
             }`
           }
         }).then(function(res) {
           if(res.data.data){
-              console.log(res.data.data.reportData.report.rankings)
             setPlayers(res.data.data.reportData.report.rankedCharacters)
+            var startTime = res.data.data.reportData.report.startTime
+            var endTime = res.data.data.reportData.report.endTime
+            console.log("Start: " + Date(startTime).toString() + " > " + startTime)
+            console.log("End: " + Date(endTime).toString() + " > " + endTime)
+            axios.request({
+              url: "api/v2/client",
+              method: "post",
+              baseURL: "https://classic.warcraftlogs.com/",
+              headers: {
+                "Authorization": `Bearer ${authToken}`
+              },
+              data:{
+                query:`{
+                  reportData {
+                    report(code:"${id}"){
+                      events(abilityID: 28520, dataType:Buffs, startTime:${0}, endTime:${100000000000}){
+                        data
+                        nextPageTimestamp
+                      }
+                    }
+                  }
+                }`
+              }
+            }).then(function(res) {
+              if(res.data.data){
+                console.log(res.data.data)
+              }
+              else{
+              }
+            })
           }
           else{
           }
