1d8987231888b609f4f675b07ed4a6cc81119324
samtrextom <46818719+samtrextom@users.noreply.github.com>
2021-10-07 16:28:09 -0500

    10.7.21.2

--- src/App.js
+++ src/App.js
@@ -31,26 +31,3 @@ function App() {
 
 export default App;
 
-//The cracks in our armor grew after years of careless ambition
-//Exposing our weaknesses and showing sides of me that I never wanted to see
-//Will all this just be an alluring memory
-//Ive been chasing for sometime now
-//Or can this be, Rebuilt, not better just different
-
-
-//Im just feeling dark blue
-//Sorry, man, its not you
-//I guess Im just like this
-//Up Late at night when
-
-//I see life in greyscale
-//When I see my plans fail
-//Sorry Im just like this
-//Please dont mind my mindset
-
-//I dont like the things i used to say
-//Im not sure why I ever felt that way
-//Moving forward is much harder
-//when the weight your pulling is a constant reminder of
-//how ive been, not where im going
-//what ive said, not how im growing
